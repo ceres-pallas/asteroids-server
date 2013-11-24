520130d93da414e5c49ef288e185430ab9d28b15
Robin van Kaathoven <robin.vankaathoven@luminis.eu>
2013-11-24 17:50:07 +0100

    Fixed viewer to display asteroids

--- options.js
+++ options.js
@@ -7,6 +7,7 @@ module.exports = {
     asteroidInitializer: function(asteroid){
 	asteroid.position({ 'x': width * Math.random(), 'y': height * Math.random() });
 	asteroid.velocity({ 'speed': '5', 'heading': 2*Math.PI * Math.random() });
+	asteroid.radius(30);
     },
     asteroidCount: 10
 }
