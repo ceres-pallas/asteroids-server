cc0bea669dc12d629a8223c5bfb05d06ed35b41e
dvberkel <daan.v.berkel.1980+git@gmail.com>
2013-11-24 17:16:04 +0100

    correct name of asteroidIntializer

--- options.js
+++ options.js
@@ -4,7 +4,7 @@ var height = 480;
 module.exports = {
     width: width,
     height: height,
-    asteroidsInitialize : function(asteroid){
+    asteroidInitializer: function(asteroid){
 	asteroid.position({ 'x': width * Math.random(), 'y': height * Math.random() });
 	asteroid.velocity({ 'speed': '5', 'heading': 2*Math.PI * Math.random() });
     },
