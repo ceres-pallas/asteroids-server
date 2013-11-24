a86daf8051c9520f24cd1e5a831383ea8f102385
dvberkel <daan.v.berkel.1980+git@gmail.com>
2013-11-24 16:21:43 +0100

    connect viewers

--- server.js
+++ server.js
@@ -1,12 +1,29 @@
 var express = require('express');
 var app = express();
 var server = require('http').createServer(app);
+var io = require('socket.io').listen(server);
 
 app.set('port', process.env.PORT || 3435);
 app.use('/start', express.static(__dirname + '/landing'));
 app.use('/control', express.static(__dirname + '/control'));
 app.get('/', function(request, response){
     response.redirect('/start/');
+});
+
+var viewers = {};
+
+io.sockets.on('connection', function(socket){
+    console.log('socket %s connected', socket.id);
+
+    socket.on('viewer', function(){
+	console.log('socket %s is a viewer', socket.id);
+	viewers[socket.id] = socket;
+    })
+
+    socket.on('disconnect', function(){
+	console.log('socket %s left the game', socket.id);
+	delete viewers[socket.id];
+    });
 });
 
 server.listen(app.get('port'));
