139d810ddfd9bd93625f74dbb241b40280060085
dvberkel <daan.v.berkel.1980+git@gmail.com>
2013-11-24 15:31:37 +0100

    serve a static landing page

--- server.js
+++ server.js
@@ -3,6 +3,10 @@ var app = express();
 var server = require('http').createServer(app);
 
 app.set('port', process.env.PORT || 3435);
+app.use('/start', express.static(__dirname + '/landing'));
+app.get('/', function(request, response){
+    response.redirect('/start/');
+});
 
 server.listen(app.get('port'));
 console.log('Asteroids server started listening on port ' + app.get('port'));
