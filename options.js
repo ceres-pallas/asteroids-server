ddca093f86cea55ed69eaae1b65528c1f38b5c5a
Daan van Berkel <daan.v.berkel.1980@gmail.com>
2014-06-24 13:20:11 +0200

    correct indent of options.js

--- options.js
+++ options.js
@@ -5,15 +5,15 @@ module.exports = {
     width: width,
     height: height,
     fighterInitializer: function(fighter){
-	fighter.position({ 'x': 50, 'y': 100 });
-	fighter.radius(10);
-	fighter.velocity({ 'speed': '0.5', 'heading': Math.PI/12 });
-	fighter.orientation(Math.PI/12);
+        fighter.position({ 'x': 50, 'y': 100 });
+        fighter.radius(10);
+        fighter.velocity({ 'speed': '0.5', 'heading': Math.PI/12 });
+        fighter.orientation(Math.PI/12);
     },
     asteroidInitializer: function(asteroid){
-	asteroid.position({ 'x': width * Math.random(), 'y': height * Math.random() });
-	asteroid.velocity({ 'speed': '5', 'heading': 2*Math.PI * Math.random() });
-	asteroid.radius(30);
+        asteroid.position({ 'x': width * Math.random(), 'y': height * Math.random() });
+        asteroid.velocity({ 'speed': '5', 'heading': 2*Math.PI * Math.random() });
+        asteroid.radius(30);
     },
     asteroidCount: 10
 }
